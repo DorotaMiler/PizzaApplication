b8b51707a6d2ce5b0a3aa9028339346113fefe44
DorotaMiler <dorota.miler@outlook.com>
2020-04-21 18:17:58 +0200

    Order creation for Builder utilization

--- src/main/java/com/dm/pizza/Order.java
+++ src/main/java/com/dm/pizza/Order.java
@@ -9,7 +9,44 @@ public class Order {
     private boolean done = false;
     private Date dueDate;
 
-    Order(long id){
+    public Order(long id) {
         this.id = id;
+    }
+
+    public Order(long id, String summary, boolean done, Date dueDate) {
+        this.id = id;
+        this.summary = summary;
+        this.done = done;
+        this.dueDate = dueDate;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Date getDueDate(Date dueDate) {
+        return new Date(dueDate.getTime());
+    }
+
+    public Date setDueDate(Date dueDate) {
+        this.dueDate = new Date(dueDate.getTime());
     }
 }
